2ee470128564ab2715417ef99abb39f7d002d357
ぱーしー <>
2018-12-10 18:23:47 +0900

    server error 500

--- MyPortfolio1/settings.py
+++ MyPortfolio1/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
      os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = "staticfiles"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'postimg')
 MEDIA_URL = "/postimg/"
